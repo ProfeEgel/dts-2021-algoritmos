03d55dd7a25213137291ca521f83bff5f312e92e
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2021-02-23 22:02:41 -0600

    binary search recursive method added

---
+++ 20210223/binary_main.py
@@ -0,0 +1,17 @@
+from search_algorithms import *
+
+data = [-11, -5, -3, 0, 1, 4, 7, 8, 9, 10, 14, 16]
+
+n = 9
+index = binary_search(data, n)
+if index >= 0:
+    print(f'{n} se encontró en la posición {index}')
+else:
+    print('No se encontró el dato')
+
+n = 7
+index = binary_search_rec(data, n)
+if index >= 0:
+    print(f'{n} se encontró en la posición {index}')
+else:
+    print('No se encontró el dato')
