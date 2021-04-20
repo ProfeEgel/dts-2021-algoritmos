68377e62973b87060a34063bfc4cdc6e8d8537fd
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2021-04-20 16:00:41 -0500

    selection sort

---
+++ 20210420/sorting_algorithms.py
@@ -0,0 +1,78 @@
+def bubble_sort(array):
+    n = len(array)
+    for j in range(0, n - 1):
+        swap_flag = False
+
+        for i in range(0, n - 1 - j):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+                swap_flag = True
+
+        if not swap_flag:
+            break
+
+
+def bubble_sort_generator(array):
+    n = len(array)
+    for j in range(0, n - 1):
+        swap_flag = False
+
+        for i in range(0, n - 1 - j):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+                swap_flag = True
+                yield array
+
+        if not swap_flag:
+            break
+
+    yield array
+
+
+def bubble_sort_debug(array):
+    comparaciones = 0
+    intercambios = 0
+
+    n = len(array)
+    for j in range(0, n - 1):
+        swap_flag = False
+
+        for i in range(0, n - 1 - j):
+            comparaciones += 1
+            if array[i] > array[i + 1]:
+                intercambios += 1
+                array[i], array[i + 1] = array[i + 1], array[i]
+                swap_flag = True
+
+        if not swap_flag:
+            break
+
+        print(f"\n***** PASADA {j}*****")
+        print(array)
+
+    print(f"\n comp={comparaciones}, inter={intercambios}")
+
+
+def selection_sort(array):
+    n = len(array)
+    for i in range(n - 1):  # pasadas
+        jmin = i
+        for j in range(i, n):  # búsqueda del mínimo
+            if array[j] < array[jmin]:
+                jmin = j
+
+        array[i], array[jmin] = array[jmin], array[i]
+
+
+def selection_sort_generator(array):
+    n = len(array)
+    for i in range(n - 1):  # pasadas
+        jmin = i
+        for j in range(i, n):  # búsqueda del mínimo
+            if array[j] < array[jmin]:
+                jmin = j
+
+        array[i], array[jmin] = array[jmin], array[i]
+        yield array
+
+    yield array
