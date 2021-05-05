4820464ebe719f2a25a6fc281da7c0164d295707
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2021-05-05 17:03:57 -0500

    QuickSort algorithm added

---
+++ 20210505/main_generators.py
@@ -0,0 +1,12 @@
+
+import random
+from sorting_animator import animate_sorting_algorithm
+from sorting_algorithms import *
+
+n = 100
+max = 100
+data = [random.randint(0,max) for i in range(n)]
+# animate_sorting_algorithm(data, n, max, bubble_sort_generator(data))
+# animate_sorting_algorithm(data, n, max, selection_sort_generator(data))
+# animate_sorting_algorithm(data, n, max, insertion_sort_generator(data))
+animate_sorting_algorithm(data, n, max, quicksort_generator(data))
